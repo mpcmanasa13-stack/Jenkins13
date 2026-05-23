7c910e8bc0edd627dc2cec562475a4e589c1d41b
mpcmanasa13-stack <mpcmanasa13@gmail.com>
2026-05-23 11:40:23 +0000

    Added Maven JSON quiz app

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -1,21 +1,60 @@
 package com.example.app;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.apache.commons.lang3.StringUtils;
+
+import java.io.FileReader;
+import java.util.Scanner;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class App {
-    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) {
-        String name = "CI/CD Pipeline";
-        if (StringUtils.isNotBlank(name)) {
-            String message = greet(name);
-            logger.info(message);
-            System.out.println(message);
-        } else {
-            logger.error("Name is empty!");
+
+        try {
+
+            JSONParser parser = new JSONParser();
+
+            JSONObject quiz =
+            (JSONObject) parser.parse(new FileReader("quiz.json"));
+
+            String question = (String) quiz.get("question");
+            String optionA = (String) quiz.get("optionA");
+            String optionB = (String) quiz.get("optionB");
+            String answer = (String) quiz.get("answer");
+
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("===== Maven Quiz App =====");
+
+            System.out.println(question);
+
+            System.out.println("A. " + optionA);
+            System.out.println("B. " + optionB);
+
+            System.out.print("Enter Answer (A/B): ");
+
+            String userAnswer = sc.nextLine();
+
+            if(userAnswer.equalsIgnoreCase(answer)){
+
+                System.out.println("Correct Answer!");
+                System.out.println("Score: 1/1");
+
+            }
+            else{
+
+                System.out.println("Wrong Answer!");
+                System.out.println("Score: 0/1");
+
+            }
+
+            System.out.println("Quiz Completed Successfully.");
+
         }
-    }
-    public static String greet(String name) {
-        return "Hello, " + name + "! Welcome to Maven CI/CD Demo.";
+        catch(Exception e){
+
+            e.printStackTrace();
+
+        }
     }
 }
